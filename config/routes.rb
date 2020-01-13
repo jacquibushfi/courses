08d24d130e6148d91c0cfa9511bdf3b974b6ceb5
jacquibushfi <jacqui@stainedglassofmckinney.com>
2020-01-13 14:03:51 -0600

    added helpers to registrations and updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'application#welcome'
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
