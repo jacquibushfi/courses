1a66c7ce7ddd57e5127de4866d28c84c567aa7e8
jacquibushfi <jacqui@stainedglassofmckinney.com>
2020-01-14 13:11:06 -0600

    consilidated line in course.rb and added nested attributes in enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,6 @@ class Course < ApplicationRecord
   has_many :enrollments
   has_many :users, through: :enrollments
 
-  validates :title, presence: true
-  validates :category, presence: true
-
+  validates :title, :category, presence: true
+  
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,6 +2,8 @@ class Enrollment < ApplicationRecord
 
   belongs_to :user
   belongs_to :course
+
+  accepts_nested_attributes_for :course
   
   validates :rating, presence: true
   validates_inclusion_of :rating, :in => 1..5
