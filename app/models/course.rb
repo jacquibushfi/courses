9bbd3b55d1e0602beefdc127352360b5f818d293
jacquibushfi <jacqui@stainedglassofmckinney.com>
2020-01-14 12:49:07 -0600

    created AR associations and validations in models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ApplicationRecord
+
+  has_many :enrollments
+  has_many :users, through: :enrollments
+
+  validates :title, presence: true
+  validates :category, presence: true
+
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,8 @@
 class Enrollment < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :course
+  
+  validates :rating, presence: true
+  validates_inclusion_of :rating, :in => 1..5
 end
