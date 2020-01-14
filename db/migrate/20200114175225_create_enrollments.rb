39848004272da7ab62280b016841cb12e3609b5d
jacquibushfi <jacqui@stainedglassofmckinney.com>
2020-01-14 11:52:45 -0600

    generated enrollment model

---
+++ db/migrate/20200114175225_create_enrollments.rb
@@ -0,0 +1,11 @@
+class CreateEnrollments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :enrollments do |t|
+      t.integer :user_id
+      t.integer :course_id
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
