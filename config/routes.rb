ab7427056d4083d6034ce9d153ff453fb6cf0e54
jacquibushfi <jacqui@stainedglassofmckinney.com>
2020-01-15 08:41:37 -0600

    begin creating New Course form and controller updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'enrollments/new'
-  get 'enrollments/show'
-  get 'enrollments/index'
-  get 'enrollments/edit'
-  get 'courses/new'
-  get 'courses/index'
-  get 'courses/show'
+
   root to: 'application#welcome'
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
 
@@ -14,6 +8,10 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new' 
     get 'signup', to: 'devise/registrations#new'
     
+  end
+
+  resources :courses, except: [:destroy] do 
+    resources :enrollments 
   end
 
 
